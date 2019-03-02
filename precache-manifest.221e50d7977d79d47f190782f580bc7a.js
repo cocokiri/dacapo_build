da9594b7f0ec1dd90f6881cfeeb3d8efa4285d97
Markus Strasser <strasser.ms@gmail.com>
2019-03-02 17:45:04 +0000

    asd

---
+++ precache-manifest.221e50d7977d79d47f190782f580bc7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc78a149b2b930e98f9d",
+    "url": "/static/css/main.30114801.chunk.css"
+  },
+  {
+    "revision": "bc78a149b2b930e98f9d",
+    "url": "/static/js/main.bc78a149.chunk.js"
+  },
+  {
+    "revision": "5f87bcfce8bd622d5851",
+    "url": "/static/js/1.5f87bcfc.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6293e2c95942c0f91c585385c65978d2",
+    "url": "/index.html"
+  }
+];
